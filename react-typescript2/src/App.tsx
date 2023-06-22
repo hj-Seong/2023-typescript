6905311914a44233ac7b619a2bef1d825adeffe6
HJ S <hjseong1222@gmail.com>
2023-06-22 10:08:39 +0900

    0622 화살표함수 컴포넌트

--- react-typescript2/src/App.tsx
+++ react-typescript2/src/App.tsx
@@ -4,6 +4,7 @@ import './App.css';
 import NoProps from './components/NoProps';
 import TypeClassComp from './components/TypeClassComp';
 import TypeFuncComp from './components/TypeFuncComp';
+import TypeArrowFuncComp from './components/TypeArrowFuncComp';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
        * 자동완성으로 쓸 수 있다 */}
       <TypeClassComp startNumber={3} name='홍길동' check/>
       <TypeFuncComp startNumber={0} />
+      <TypeArrowFuncComp startNumber={5}/>
     </div>
   );
 }
