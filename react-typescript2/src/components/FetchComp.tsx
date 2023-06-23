a6b1ef58cf6370d411adb3b05af30fb3bc1130ba
HJ S <hjseong1222@gmail.com>
2023-06-23 10:45:55 +0900

    0623 FetchAPI-2

--- react-typescript2/src/components/FetchComp.tsx
+++ react-typescript2/src/components/FetchComp.tsx
@@ -13,6 +13,7 @@ interface dataBusan {
 export default function FetchComp() {
 
   const [data, setData] = useState<dataBusan>()
+  const [dataArray, setDataArray] = useState<dataBusan[]>()
 
 
   // FetchAPI를 사용할때 외부(서버)
@@ -23,6 +24,14 @@ export default function FetchComp() {
     // await를 이용하여 값 가져오기
     const response = await promise.json();
     console.log(response.body[0])
+
+    setData({
+        AREA_NM: response.body[0].AREA_NM,
+        MENU_NM: response.body[0].MENU_NM,
+        RSTR_NM: response.body[0].RSTR_NM,
+    })
+    setDataArray(response.body)
+
   }
   // 한번만 데이터 가져옴
   useEffect(()=>{
@@ -32,6 +41,22 @@ export default function FetchComp() {
   return (
     <div>
         <h3>FetchComp</h3>
+        <div>{data &&
+            <div>
+                <p>{data.AREA_NM}</p>
+                <p>{data.RSTR_NM}</p>
+                <p>{data.MENU_NM}</p>
+            </div> 
+         }</div>
+
+        {
+            // 현재의 타입이 undefind나 다른 타입이 있을때
+            // ?를 통해서 현재의 타입이 배열일때만 실행
+            dataArray?.map((d)=>(
+                <li>{d.AREA_NM}</li>
+            ))
+        }
     </div>
+
   )
 }
